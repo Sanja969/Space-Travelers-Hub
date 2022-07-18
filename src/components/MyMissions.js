6d6a5525924a97bf68ca975ef1f4ae8217f7e0ea
Sanja <sanja1mandic@gmail.com>
2022-07-19 00:52:45 +0200

    Make empty MyMissions component in order to import in in myPage and to give it position

---
+++ src/components/MyMissions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyMissions() {
+  const missions = 'My Missions';
+  return (
+    <div className="myMissions w-50">
+      <h3>{missions}</h3>
+    </div>
+  );
+}
+
+export default MyMissions;

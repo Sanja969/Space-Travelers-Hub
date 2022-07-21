5a3c18f6eb04c6341d5e777f7b2b6dc42dfd0d3d
Sanja <sanja1mandic@gmail.com>
2022-07-21 10:27:52 +0200

    deleted unnecessary useSelector

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,11 @@
 /*eslint-disable*/
 
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Singlemission from './MissionList';
 import { Setmissions } from '../Redux/missions-redux';
 
 const Missions = () => {
-  // const mission = useSelector((state) => state.missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
